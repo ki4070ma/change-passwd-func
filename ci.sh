3e27ae22420436f37d849246f850b371b3b92d56
Mori Atsushi <ki4070ma@gmail.com>
2020-05-17 20:32:36 +0900

    Added setup.cfg and deleted each conf files (#107)

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ if ! python -m isort --check-only -rc . ; then
   EXIT_STATUS=1
 fi
 
-if ! python -m autopep8 -r --global-config .config-pep8 -i . ; then
+if ! python -m autopep8 -r -i . ; then
   echo "Please run command 'python -m autopep8 -r --global-config .config-pep8 -i .' on your local and commit the result"
   EXIT_STATUS=1
 fi
